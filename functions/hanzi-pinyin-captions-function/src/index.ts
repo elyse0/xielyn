d656252fdfa7dc7ea46db5ebc2b6719930b3c25b
Elyse <26639800+elyse0@users.noreply.github.com>
2022-08-26 15:51:47 -0500

    Enable cors for hanzi-pinyin-caption GCP Function

--- functions/hanzi-pinyin-captions-function/src/index.ts
+++ functions/hanzi-pinyin-captions-function/src/index.ts
@@ -1,21 +1,30 @@
 import { Request, Response } from '@google-cloud/functions-framework'
+import cors from 'cors'
 
 import { getHanziPinyinCaptions } from '@xielyng/hanzi-pinyin-captions'
 
+const corsMiddleware = cors();
+
 const main = (req: Request, res: Response) => {
-    if (req.method !== 'POST') {
-        return res.status(501).send({
-            error: `Method ${req.method} is not supported`,
-        });
-    }
+    corsMiddleware(req, res, () => {
+        if (req.method === 'OPTIONS') {
+            return res.status(204)
+        }
 
-    if (!req.body || !Object.keys(req.body).length) {
-        return res.status(501).send({
-            error: 'Provide video captions inside body request',
-        })
-    }
+        if (req.method !== 'POST') {
+            return res.status(501).send({
+                error: `Method ${req.method} is not supported`,
+            });
+        }
 
-    return res.status(200).send(getHanziPinyinCaptions(req.body))
+        if (!req.body || !Object.keys(req.body).length) {
+            return res.status(400).send({
+                error: 'Provide video captions inside body request',
+            })
+        }
+
+        return res.status(200).send(getHanziPinyinCaptions(req.body))
+    })
 }
 
 export { main }
