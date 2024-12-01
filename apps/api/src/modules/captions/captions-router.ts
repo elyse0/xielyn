913815a90458f92fd5aa7fc661b229200a412c3e
Elyse <26639800+elyse0@users.noreply.github.com>
2024-12-01 14:24:25 -0600

    refactor: use jieba-wasm in the browser to generate pinyin

--- apps/api/src/modules/captions/captions-router.ts
+++ apps/api/src/modules/captions/captions-router.ts
@@ -1,13 +1,8 @@
 import express from "express";
 
-import generateHanziPinyinCaptionsController from '@/modules/captions/use-cases/generate-hanzi-pinyin-captions/index.js'
 import getYoutubeCaptionsController from '@/modules/captions/use-cases/get-youtube-captions/index.js'
 
 const router = express.Router() as express.Router;
-
-router.post("/generate-hanzi-pinyin", (req: express.Request, res: express.Response) => {
-    return generateHanziPinyinCaptionsController.execute(req, res)
-});
 
 router.get("/youtube/:videoId/:languageId?", (req: express.Request, res: express.Response) => {
     return getYoutubeCaptionsController.execute(req, res)
--- apps/api/src/modules/captions/use-cases/generate-hanzi-pinyin-captions/index.ts
+++
@@ -1,5 +0,0 @@
-import GenerateHanziPinyinCaptionsController from '@/modules/captions/use-cases/generate-hanzi-pinyin-captions/GenerateHanziPinyinCaptionsController.js'
-
-const generateHanziPinyinCaptionsController = new GenerateHanziPinyinCaptionsController()
-
-export default generateHanziPinyinCaptionsController
